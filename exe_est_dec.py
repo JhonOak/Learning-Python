6c1c299ce9bbba75a7b0ed44bf4f089ad980cee2
JhonOak <121194711+JhonOak@users.noreply.github.com>
2023-02-17 13:52:24 -0300

    Update exe_est_dec.py

--- exe_est_dec.py
+++ exe_est_dec.py
@@ -210,9 +210,78 @@ else:
     print("Valor inválido")
 
 # 14 - Notas semestrais, média, conceito e aprovado/reprovado
-sem_1 = float(input("Qual foi a 1ª nota no Semestre? "))
-sem_2 = float(input("Qual foi a 2ª nota no Semestre? "))
-sem_m = (sem_1 +sem_2) / 2
+sem = [float(input("Qual foi a 1ª nota no Semestre? ")), float(input("Qual foi a 2ª nota no Semestre? "))]
+sem_m = round((sem[0] +sem[1]) / 2, 2)
 cnct = ["A", "B", "C", "D", "E"]
-apvd = False
-if sem_m 
+aprp = ["Aprovado", "Reprovado"]
+if sem_m >= 9 and sem_m <= 10:
+  print("Suas notas foram:", sem[0], "e", sem[1])
+  print("Sua média foi   :", sem_m)
+  print("Seu conceito foi:", cnct[0])
+  print("Você foi        :", aprp[0])
+elif sem_m >= 7.5 and sem_m < 9:
+  print("Suas notas foram:", sem[0], "e", sem[1])
+  print("Sua média foi   :", sem_m)
+  print("Seu conceito foi:", cnct[1])
+  print("Você foi        :", aprp[0])
+elif sem_m >= 6 and sem_m < 7.5:
+  print("Suas notas foram:", sem[0], "e", sem[1])
+  print("Sua média foi   :", sem_m)
+  print("Seu conceito foi:", cnct[2])
+  print("Você foi        :", aprp[0])
+elif sem_m >= 4 and sem_m < 6:
+  print("Suas notas foram:", sem[0], "e", sem[1])
+  print("Sua média foi   :", sem_m)
+  print("Seu conceito foi:", cnct[3])
+  print("Você foi        :", aprp[1])
+elif sem_m >= 0 and sem_m < 4:
+  print("Suas notas foram:", sem[0], "e", sem[1])
+  print("Sua média foi   :", sem_m)
+  print("Seu conceito foi:", cnct[4])
+  print("Você foi        :", aprp[1])
+else:
+  print("Valor inválido")
+
+# 15 - Peça três lados de um triângulo, exiba se é possível e qual é o tipo de triângulo
+print("Digite os três lados do triângulo: ")
+tri = [float(input()), float(input()), float(input())]
+if tri[0] + tri[1] > tri[2] and tri[0] + tri[2] > tri[1] and tri[1] + tri[2] > tri[0]:
+  print("É possível um triângulo com esses valores")
+  if tri[0] == tri[1] == tri[2]:
+    print("Tipo de Triângulo: Equilátero")
+  elif tri[0] == tri[1] or tri[0] == tri[2] or tri[1] == tri[2]:
+    print("Tipo de Triângulo: Isósceles")
+  elif tri[0] != tri[1] != tri[2]:
+    print("Tipo de Triângulo: Escaleno")
+elif tri[0] + tri[1] <= tri[2] or tri[0] + tri[2] <= tri[1] or tri[1] + tri[2] <= tri[0]:
+  print("Não é possível um triângulo com esses valores")
+else:
+  print("Valores inválidos")
+
+# 16 - Calculadora de raízes de equação do 2º grau
+import math
+print("Digite os valores de a, b e c para uma equação ax² + bx + c")
+a = float(input())
+b = float(input())
+c = float(input())
+delta = (b ** 2) - (4 * a * c)
+x_1 = (-b + math.sqrt(delta)) / 2 * a
+x_2 = (-b - math.sqrt(delta)) / 2 * a
+if a == 0:
+  print("Esta equação não é do segundo grau.")
+elif b == 0:
+  x_1 = math.sqrt(-c / a)
+  x_2 = - math.sqrt(-c / a)
+  print(f"As raízes desta equação são {x_1} e {x_2}.")
+elif c == 0:
+  x_1 = 0
+  x_2 = -b / a
+  print(f"As raízes desta equação são {x_1} e {x_2}.")
+elif delta < 0:
+  print("Esta equação não possui raizes reais.")
+elif delta == 0:
+  print(f"Esta equação possui apenas uma raiz, que é {x_1}.")
+elif delta > 0:
+  print(f"As raízes desta equação são {x_1} e {x_2}.")
+else:
+  print("Valores Inválidos")
