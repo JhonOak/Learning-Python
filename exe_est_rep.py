aeec5073181b97c4f977b0638d0a8d90d953b990
JhonOak <121194711+JhonOak@users.noreply.github.com>
2023-02-27 13:31:48 -0300

    Update exe_est_rep.py

--- exe_est_rep.py
+++ exe_est_rep.py
@@ -1,3 +1,6 @@
+# Exercícios de Estrutura de Repetição:
+# https://wiki.python.org.br/EstruturaDeRepeticao
+
 # 1
 nta = float(input("Digite uma nota de 0 a 10: "))
 while 0 > nta > 10:
@@ -52,3 +55,65 @@ d - Divorciad{gen}
 
 Estado Civil: '''))
 
+# 4
+pop = [80000, 200000, 0.03, 0.015, 0]
+while pop[0] < pop[1]:
+  pop[4] += 1
+  pop[0] = pop[0] + (pop[0] * pop[2])
+  pop[1] = pop[1] + (pop[1] * pop[3])
+print(f"Levará {pop[4]} anos para População do País A ultrapassar a do País B.")
+
+# 5 Incompleto, pulei, volto depois kkk
+#pop = [float(input("Digite a População do País A: ")), float(input("Digite a População do País B: ")), float(input("Digite em Porcentagem a Taxa de Crescimento do País A: ")), float(input("Digite em Porcentagem a Taxa de Crescimento do País B: ")), 0]
+# while pop[0] < pop[1]:
+#  pop[4] = pop[4] + 1
+#  pop[0] = pop[0] + (pop[0] * pop[2] / 100)
+#  pop[1] = pop[1] + (pop[1] * pop[3] / 100)
+#print(f"Levará {pop[4]} anos para População do País A ultrapassar a do País B.")
+
+while True:
+  txt = ["A", "B", "Digite a População do País ", "Digite em Porcentagem a Taxa de Crescimento do País "]
+  pop = [float(input(f"{txt[2]}{txt[0]}: ")), float(input(f"{txt[2]}{txt[1]}: ")), float(input(f"{txt[3]}{txt[0]}: ")), float(input(f"{txt[3]}{txt[0]}: ")), 0]
+  if pop[0] < pop[1] and pop[2] < pop[3]:
+    print("País A nunca vai ultrapassar o País B com uma Taxa de Crescimento menor.")
+  elif pop[0] > pop[1] and pop[2] > pop[3]:
+    print("País B nunca vai ultrapassar o País A com uma Taxa de Crescimento menor.")
+  pop[4] = pop[4] + 1
+  pop[0] = pop[0] + (pop[0] * pop[2] / 100)
+  pop[1] = pop[1] + (pop[1] * pop[3] / 100)
+  if pop[0] < pop[1] and pop[2] > pop[3]:
+    print(f"Levará {pop[4]} anos para População do País A ultrapassar a do País B.")
+  elif pop[0] > pop[1] and pop[2] < pop[3]:
+    print(f"Levará {pop[4]} anos para População do País B ultrapassar a do País A.")
+    
+ # 6
+num = 0
+while num < 20:
+  num += 1
+  print(num, end = ' ') # <- Exibe na horizontal       print(num) <- Exibe na vertical
+  
+# 9
+for num in range(1, 50, 2):
+  print(num)
+  
+# 10
+n = [int(input("Digite o 1º número:")), int(input("Digite o 2º número:"))]
+for num in range(n[0], n[1]+1):
+  print(num)
+ 
+# 11
+n = [int(input("Digite o 1º número: ")), int(input("Digite o 2º número: "))]
+sum = 0
+for num in range(n[0], n[1]+1):
+  sum += num
+  print(num)
+print(f"Soma dos números gerados: {sum}") 
+
+# 12
+num = int(input("Digite um número no qual deseja saber a tabuada: "))
+cnt = 0
+while cnt < 10:
+  cnt += 1
+  tab = num * cnt
+  print(f"{num} x {cnt} = {tab}")
+  
